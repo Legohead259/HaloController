46d74f601f04f5f01b70db9d7d69aec530715b7a
Braidan <legohead259@gmail.com>
2017-11-25 00:03:02 -0500

    Standardized led_bar_demo.py to include a try/finally block

--- demo/led_bar_demo.py
+++ demo/led_bar_demo.py
@@ -1,22 +1,26 @@
 import Control_Interfaces.led_bar as led_bar
 import time
 
-led_bar.setup()
+try:
+    led_bar.setup()
 
-boot_start = time.time()
-print boot_start  # Debug
+    boot_start = time.time()
+    print boot_start  # Debug
 
-while time.time() <= boot_start + 10:
-    led_bar.spectrum_scroll()
+    while time.time() <= boot_start + 10:
+        led_bar.spectrum_scroll()
 
-link_start = time.time()
-print link_start  # Debug
+    link_start = time.time()
+    print link_start  # Debug
 
-while time.time() <= link_start + 5:
-    led_bar.alternate_flash(delay=0.125)
+    while time.time() <= link_start + 5:
+        led_bar.alternate_flash(delay=0.125)
 
-ack_start = time.time()
-print ack_start  # Debug
+    ack_start = time.time()
+    print ack_start  # Debug
 
-while time.time() <= ack_start + 3:
-    led_bar.flash(led_bar._green_leds)
+    while time.time() <= ack_start + 3:
+        led_bar.flash(led_bar._green_leds)
+
+finally:
+    led_bar.clean()
